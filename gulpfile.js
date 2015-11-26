b3ccbfd2fc06e6515bd906b0b2f6ed6e1ea16e38
Scott <dev@misonline.biz>
2015-11-26 01:12:58 -0500

    Updated font library

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ gulp.task('default',function(){
     console.log('echo material design icons...');
 });
 
+
+
 gulp.task('sassdoc', function () {
     return gulp.src('stylesheets/**/*.scss')
         .pipe(sassdoc());
